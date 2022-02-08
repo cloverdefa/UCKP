c6d876cb3ce63d013d8391ce65c979ea3a067c30
cloverdefa <cloverdefa@users.noreply.github.com>
2022-02-08 09:53:40 +0800

    添加安裝gcp

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt full-upgrade -y #更新APT庫&更新內容
-sudo apt install curl snmpd nano bash-completion wakeonlan -y  #下載安裝 curl&snmpd&bash-completion&wakeonlan 四項
+sudo apt install curl snmpd gcp nano bash-completion wakeonlan -y  #下載安裝 curl&snmpd&bash-completion&wakeonlan 四項
 sudo mv /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.bak  #備份預設snmpd.conf
 sudo cp /home/$(whoami)/uckh/snmpd.conf /etc/snmp/  #複製下載編輯完成的snmpd.conf至/etc/snmp/
 sudo curl -o /usr/bin/distro https://raw.githubusercontent.com/librenms/librenms-agent/master/snmp/distro  #下載distro
