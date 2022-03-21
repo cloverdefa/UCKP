12c8f6d5749d2cd79f2136cdef59a828e90994e8
cloverdefa <39210648+cloverdefa@users.noreply.github.com>
2022-03-21 08:07:11 +0800

    資料夾名稱修正

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update && sudo apt full-upgrade -y #更新APT庫&更新內容
 sudo apt install curl nano bash-completion wakeonlan -y  #下載安裝 curl&bash-completion&wakeonlan
-sudo rm -r ~/uckh  #移除下載資料夾
+sudo rm -r ~/uckp  #移除下載資料夾
 sudo reboot  #重新啟動開機
